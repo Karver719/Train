6b8606bb6d5ad653ee2203417e06d0c7cc9e6640
Andrey <afonin-andrey2017@yandex.ru>
2024-01-30 18:59:34 +0300

    as

--- data.py
+++ data.py
@@ -4,18 +4,14 @@ import json
 
 app = Flask(__name__)
 
-# Подключение к RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-# Объявление обменника
 channel.exchange_declare(exchange='train_num', exchange_type='fanout')
 
-# Создание временной очереди
 result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue
 
-# Привязка очереди к обменнику
 channel.queue_bind(exchange='train_num', queue=queue_name)
 
 @app.route('/')
@@ -27,7 +23,6 @@ def stream():
     def generate():
         for method_frame, properties, body in channel.consume(queue_name, inactivity_timeout=1):
             if body is not None:
-                # Преобразование строки JSON в словарь
                 data = json.loads(body)
                 yield f"data: {json.dumps(data)}\n\n"
 
--- server_data.py
+++ server_data.py
@@ -5,14 +5,11 @@ import json
 from datetime import datetime
 import psycopg2
 
-# Подключение к RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-# Объявление обменника
 channel.exchange_declare(exchange='train_num', exchange_type='fanout')
 
-# Подключение к PostgreSQL
 db_connection = psycopg2.connect(
     host='localhost',
     user='postgres',
@@ -22,31 +19,24 @@ db_connection = psycopg2.connect(
 db_cursor = db_connection.cursor()
 
 while True:
-    # Генерация рандомного числа
     entrance_track = random.randint(1, 2)
     exit_track = random.randint(1, 2)
     train_length = random.randint(1, 4)
     timestamp = datetime.now()
 
-    # Создание словаря
     data = {'входной парк': entrance_track,
             'выходной парк': exit_track,
             'длина поезда': train_length}
 
-    # Преобразование словаря в строку JSON
     json_data = json.dumps(data)
 
-    # Отправка JSON на обменник
     channel.basic_publish(exchange='train_num', routing_key='', body=json_data)
     print(f"Отправлено: {json_data}")
 
-    # Запись данных в PostgreSQL
     db_cursor.execute("INSERT INTO train_logs (entrance_track, exit_track, train_length, timestamp) VALUES (%s, %s, %s, %s)",
                       (entrance_track, exit_track, train_length, timestamp))
     db_connection.commit()
 
-    # Задержка в секундах (может быть изменено в зависимости от требований)
-    time.sleep(1)
+    time.sleep(4)
 
-# Закрытие соединения
 connection.close()
